8f3d93ce850a8feee11c8b77971c2f7e27e364ef
픽셀강박 <126048017+denshol@users.noreply.github.com>
2024-02-23 07:17:13 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    var answer = [];
+    for(let j=0;j<arr.length;j++){
+        
+    
+    for(let i=0;i<arr[j];i++){
+        answer.push(arr[j]);
+    }
+}
+    return answer;
+}
