0728a475d1ee0690d9fc4a33142430d7bb0b6795
픽셀강박 <126048017+denshol@users.noreply.github.com>
2024-02-24 04:26:55 +0900

    [Bronze II] Title: 16진수, Time: 100 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main{
+    
+
+public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    String n = sc.nextLine();
+  
+   int decimal = Integer.parseInt(n,16);
+    System.out.print(decimal);
+}
+    
+}
